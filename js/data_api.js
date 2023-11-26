9d50eaea1cb7872f1d4f3e2177b9bd3728128dc0
zobirofkir <zobirofkir19@gmail.com>
2023-11-26 20:46:00 +0100

    Complet peoject search Movies Api ...

--- js/data_api.js
+++ js/data_api.js
@@ -3,6 +3,110 @@ const url = "https://api.themoviedb.org/3";
 const base_image = "https://image.tmdb.org/t/p/w500";
 const base_url = "/discover/movie";
 
+const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzYjM1OTAzZWNlMDM0MTc3MDEwMWQzNGM2NDdmMmFjOSIsInN1YiI6IjY1NjExODc3NDk3NTYwMDBjNjJmMjMwZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.cyE3__R6ibWn4TQnJhkHhSxa7LHzN5IRyPAsOYe7AUA'
+    }
+  };
+
+  const searchButton = document.getElementById("searchButton");
+  searchButton.addEventListener('submit', function(e){
+    e.preventDefault();
+    const urlSearch = `${url}/search/movie?query=${this.search.value}`;
+
+    const searched = document.getElementById("apiSearch");
+    fetch(urlSearch, options)
+    .then(response => response.json())
+    .then(json => {
+        (json.results).map(result=>{
+            const imageUrlCsw = result.poster_path
+            ? `${base_image}${result.poster_path}`
+            : "path_to_default_image"; // Replace "path_to_default_image" with the path to your default image
+
+            searched.innerHTML += `
+            <div class="custom-block d-flex">
+            <!-- Left Section (Image and Title) -->
+            <div class="">
+                <div class="custom-block-icon-wrap">
+                    <div class=""></div>
+                    <a href="">
+                        <img src="${imageUrlCsw}" class="custom-block-image img-fluid" alt="${result.title}">
+                    </a>
+                </div>
+                <div class="mt-2">
+                    <a href="#" class="btn custom-btn">
+                        ${result.original_title}
+                    </a>
+                </div>
+            </div>
+        
+            <!-- Middle Section (Details) -->
+            <div class="custom-block-info">
+                <div class="custom-block-top d-flex mb-1">
+                    <small class="me-4">
+                        <i class="bi-clock-fill custom-icon"></i>
+                        ${result.original_language}
+                    </small>
+                </div>
+        
+                <h5 class="mb-2">
+                    <a href="detail-page.html">
+                        ${result.original_title}
+                    </a>
+                </h5>
+        
+                <div class="profile-block d-flex">
+                    <img src="images/profile/woman-posing-black-dress-medium-shot.jpg" class="profile-block-image img-fluid" alt="">
+                    <p>
+                        ${result.title}
+                        <img src="images/verified.png" class="verified-image img-fluid" alt="">
+                        <strong>${result.release_date}</strong>
+                    </p>
+                </div>
+        
+                <p class="mb-0">${result.overview}</p>
+        
+                <div class="custom-block-bottom d-flex justify-content-between mt-3">
+                    <a href="#" class="">
+                        <i class="fa-solid fa-star fa-bounce"></i>
+                        <span>${result.vote_average}</span>
+                    </a>
+        
+                    <a href="#" class="">
+                        <i class="fa-solid fa-video fa-bounce"></i>
+                        <span>${result.video}</span>
+                    </a>
+        
+                    <a href="#" class="">
+                        <i class="fa-regular fa-user fa-bounce"></i>
+                        <span>${result.genre_ids}</span>
+                    </a>
+                </div>
+            </div>
+        
+            <!-- Right Section (Badges) -->
+            <div class="d-flex flex-column ms-auto">
+                <a href="#" class="badge ms-auto">
+                    <i class="bi-heart"></i>
+                </a>
+                <a href="#" class="badge ms-auto">
+                    <i class="bi-bookmark"></i>
+                </a>
+            </div>
+        </div>
+        <br>
+        `;
+
+        });
+    })
+  });
+  
+
+
+
+
 async function fetchZobir(datas) {
     try {
         const response = await fetch(datas);
